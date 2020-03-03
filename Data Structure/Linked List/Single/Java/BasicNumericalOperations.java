c6925996dd00b1d965ba8ea3dc39443d06a190f4
suhel-pal <61659841+suhel-pal@users.noreply.github.com>
2020-03-04 00:51:01 +0530

    Add functionality Completed

--- Data Structure/Linked List/Single/Java/BasicNumericalOperations.java
+++ Data Structure/Linked List/Single/Java/BasicNumericalOperations.java
@@ -34,20 +34,85 @@ public class BasicNumericalOperations {
 			}
 		}
 
-		SingleLinkList firstNumber =  convertStringToNumber(firstNumberString);
-		SingleLinkList secondNumber =  convertStringToNumber(secondNumberString);
-		System.out.println("After converting into list first number:");
-		firstNumber.printElements();
-		System.out.println("After converting into list second number:");
-		secondNumber.printElements();
+		SingleLinkList result = addTwoNumbers(firstNumberString, secondNumberString);
+		System.out.println("Summation of two given number is");
+		result.printElements();
 
 	}
-	
-	public static SingleLinkList convertStringToNumber(String number) {
+
+	private static SingleLinkList addTwoNumbers(String firstNumberString, String secondNumberString) {
+
+		SingleLinkList firstNumber = convertStringToNumber(firstNumberString); 
+		SingleLinkList secondNumber = convertStringToNumber(secondNumberString); 
+
+		SingleLinkList addResult = new SingleLinkList();
+
+		firstNumber = reverseList(firstNumber);
+		secondNumber = reverseList(secondNumber);
+
+		Node head1 = firstNumber.getHead();
+		Node head2 = secondNumber.getHead();
+
+		int carry = 0;
+		int result = 0;
+
+		while ((head1 != null || head2 != null) || carry!=0) {
+			if (head1 != null && head2 != null) {
+				result = Integer.parseInt(head1.getData()) + Integer.parseInt(head2.getData()) + carry;
+				carry = result / 10;
+				result = result % 10;
+				addResult.addNode(String.valueOf(result));
+				head1 = head1.getLink();
+				head2 = head2.getLink();
+			} else if (head1 == null && head2 != null) {
+				result = Integer.parseInt(head2.getData()) + carry;
+				carry = result / 10;
+				result = result % 10;
+				addResult.addNode(String.valueOf(result));
+				head2 = head2.getLink();
+			} else if (head1 != null && head2 == null) {
+				result = Integer.parseInt(head1.getData()) + carry;
+				carry = result / 10;
+				result = result % 10;
+				addResult.addNode(String.valueOf(result));
+				head1 = head1.getLink();
+			} else if (head1 == null && head2 == null && carry != 0) {
+				addResult.addNode(String.valueOf(carry));
+				carry = 0;
+			}
+		}
+
+		return reverseList(addResult);
+	}
+    
+	/***
+	 * Algorithm for reversing the list
+	 * current = head , previous = null , next = null
+	 * while(current!=null) do
+	 *   next = current.next
+	 *   current.next = previous
+	 *   previous = current
+	 *   current = next;
+	 * End While  
+	 */
+	private static SingleLinkList reverseList(SingleLinkList list) {
+		Node current = list.getHead();
+		Node previous = null, next = null;
+		while(current!=null) {
+			next = current.getLink();
+			current.setLink(previous);
+			previous = current;
+			current = next;
+		}
 		
+		return new SingleLinkList(previous);
+	}
+
+	private static SingleLinkList convertStringToNumber(String number) {
+
 		SingleLinkList list = new SingleLinkList();
-		for(int index=0; index<number.length() ; index++ ) {
-			list.addNode(number.substring(index, index+1));
+		for (int index = 0; index < number.length(); index++) {
+			list.addNode(number.substring(index, index + 1));
 		}
 		return list;
 	}
